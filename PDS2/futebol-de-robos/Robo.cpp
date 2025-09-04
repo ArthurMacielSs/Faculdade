946db8966e6fef390b2582b796d57d0504d6dddb
Arthur Maciel <arthurmacieldesilva@gmail.com>
2025-09-04 19:01:50 -0300

    finishing tad

--- PDS2/futebol-de-robos/Robo.cpp
+++ PDS2/futebol-de-robos/Robo.cpp
@@ -1,43 +1,86 @@
-#include <iostream>
-#include "Ponto2D.hpp"
+#include "Robo.hpp" 
+#include <iostream>  
+#include <cmath>
 
-using namespace std;
 
-struct Robo{
+struct Robo {
+    // --- Membros ---
     double _energia;
     bool _com_bola;
     int id;
     Ponto2D position;
+Robo(int i, const Ponto2D& pos, bool bol) : _energia(100), _com_bola(bol),id(i), position(pos){}
+
+void mover(double v, double th, double t) {
+
+    double vx = v * cos(th); 
+    double vy = v * sin(th); 
+    double desloc_x = vx * t;
+    double desloc_y = vy * t;
+    this->position.px += desloc_x;
+    this->position.py += desloc_y;
+    double distancia = v * t;
     
 
-Robo (int i, Ponto2D ponto, bool bol){
-    this->id= i;
-    this->_com_bola= bol;
-    this->position.px=ponto.px;
-    this->position.py=ponto.py;
-    this->_energia=100;
-};
-
-void mover (double v,double th, double t){
-
-
-};
-
-double calcular_distancia (Robo * robo){
-    double dist = this->position.calcular_distancia(robo->position);
-    return dist;
-};
-
-Robo * determinar_robo_mais_proximo(Robo** naves, int n){
-
-};
-
-void passar_bola(Robo ** time, int n){
-
+    if (this->_energia<=0){
+        this->_energia=0;
+    }
+    else {
+        this->_energia-=distancia;
+    }
 }
 
-void imprimir_status(){
+double calcular_distancia(Robo* robo) {
+    double distancia = this->position.calcular_distancia(&robo->position);
+    return distancia;
+}
 
-};
+Robo* determinar_robo_mais_proximo(Robo** naves, int n) {
+    Robo* teste;
+    double distancia1=  this->calcular_distancia(naves[0]);
+    double distancia2 = distancia1;
+    double menordistancia=0;
+    
+    for(int i=0; i<n; i++){
+    if(naves[i]->id==this->id){
+        continue;
+    }
+    else{
+       distancia1 = this->calcular_distancia(naves[i]);
+       if(distancia1<distancia2){
+        menordistancia= distancia1;
+        teste = &*naves[i];
+       }
+       distancia2= distancia1;
 
+       
+
+    }
+    }
+    return teste;
+    
+}
+
+void passar_bola(Robo** time, int n) {
+    Robo *teste;
+    for(int i=0; i<n; i++){
+
+        if(this->_com_bola){
+           teste=determinar_robo_mais_proximo(time, n);
+           this->_com_bola= false;
+           teste->_com_bola = true;
+
+        }
+        else {
+            cin >> "Estou sem a bola!\n";
+        }
+    }
+
+  
+}
+
+void imprimir_status() {
+ 
+    cin >> id>> "\t">> this->position.px>> "\t" >>this->position.py >> "\t" >> this->_energia>> "\n";
+}
 };
--- PDS2/futebol-de-robos/Robo.hpp
+++ PDS2/futebol-de-robos/Robo.hpp
@@ -1,25 +1,26 @@
-#ifndef Robo_h
-#define Robo_h
+#ifndef ROBO_HPP
+#define ROBO_HPP
 
-using namespace std;
+#include "Ponto2D.hpp" // Inclui a definição de Ponto2D
 
-struct Robo{
+struct Robo {
+    // --- Membros ---
     double _energia;
     bool _com_bola;
-    
+    int id;
+    Ponto2D position;
 
-Robo (int, Ponto2D, bool);
+    Robo(int i, const Ponto2D& pos, bool bol);
 
-void mover (double v,double th, double t);
+    void mover(double v, double th, double t);
 
-double calcular_distancia (Robo * robo);
+    double calcular_distancia(Robo* robo);
 
-Robo * determinar_robo_mais_proximo(Robo** naves, int n);
+    Robo* determinar_robo_mais_proximo(Robo** naves, int n);
 
-void passar_bola(Robo ** time, int n);
+    void passar_bola(Robo** time, int n);
 
-void imprimir_status();
-
+    void imprimir_status();
 };
 
 #endif
